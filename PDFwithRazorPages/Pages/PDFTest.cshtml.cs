45f60266d14cfbc3a797310bef36d6187acdc86f
Takayuki Ogawa <ogavva.t@gmail.com>
2020-05-18 18:26:48 +0900

    first commit

---
+++ PDFwithRazorPages/Pages/PDFTest.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Wkhtmltopdf.NetCore;
+using PDFwithRazorPages.Models;
+
+namespace PDFwithRazorPages.Pages
+{
+    public class PDFTestModel : PageModel
+    {
+        readonly IGeneratePdf _generatePdf;
+        public string Title {get; set;}
+
+        public PDFTestModel(IGeneratePdf generatePdf)
+        {
+            _generatePdf = generatePdf;
+        }
+
+        public void OnGet()
+        {
+            Title = "PDF Test 222";
+            return;
+        }
+    }
+}
